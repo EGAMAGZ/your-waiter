bf64abc500022c1638f533e53e11e427ae3461aa
EGAMAGZ <46827955+EGAMAGZ@users.noreply.github.com>
2024-09-18 04:46:32 -0600

    fix: supabaseurl required

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,10 @@
 import type { Database } from "@/util/database.types";
 import { createClient } from "@supabase/supabase-js";
 
+const SUPABASE_URL = import.meta.env.SUPABASE_URL || process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.SUPABASE_ANON_KEY || process.env.SUPABASE_ANON_KEY; 
+
 export const supabase = createClient<Database>(
-  import.meta.env.SUPABASE_URL as string,
-  import.meta.env.SUPABASE_ANON_KEY as string,
+  SUPABASE_URL as string,
+  SUPABASE_ANON_KEY as string,
 );
